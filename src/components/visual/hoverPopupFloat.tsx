864a53d7faf641204a5b6edc26227e603bd7fb9f
James Lyons <jamwillyo@gmail.com>
2024-08-13 15:29:49 +0000

    add offset to props and also moved hover events

--- src/components/visual/hoverPopupFloat.tsx
+++ src/components/visual/hoverPopupFloat.tsx
@@ -4,16 +4,27 @@ import React, { ReactNode, useState } from "react";
 interface HoverPopupProps {
 	itemToHover: ReactNode;
 	itemToPopUp: ReactNode;
+	xOffset?: number;
+	yOffset?: number;
 }
 
 const HoverPopup: React.FC<HoverPopupProps> = ({
 	itemToHover,
 	itemToPopUp,
+	xOffset = 0,
+	yOffset = 0,
 }) => {
 	const [isHovered, setIsHovered] = useState(false);
+	const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
 
-	const handleMouseEnter = () => {
+	const handleMouseEnter = (e: { clientX: any; clientY: any }) => {
+		if (isHovered) return;
 		setIsHovered(true);
+
+		setMousePosition({
+			x: e.clientX,
+			y: e.clientY,
+		});
 	};
 
 	const handleMouseLeave = () => {
@@ -21,19 +32,23 @@ const HoverPopup: React.FC<HoverPopupProps> = ({
 	};
 
 	return (
-		<div
-			className=""
-			onMouseEnter={handleMouseEnter}
-			onMouseLeave={handleMouseLeave}
-		>
-			{itemToHover}
+		<div className="" onMouseLeave={handleMouseLeave}>
+			<div onMouseEnter={handleMouseEnter}>{itemToHover}</div>
 
 			<div
 				className={`transition-all duration-500 ${
 					isHovered ? "fixed opacity-100" : "hidden opacity-0"
 				}`}
 			>
-				{itemToPopUp}
+				<div
+					style={{
+						position: "fixed",
+						left: `${mousePosition.x + xOffset - 2}px`,
+						top: `${mousePosition.y + yOffset - 2}px`,
+					}}
+				>
+					{itemToPopUp}
+				</div>
 			</div>
 		</div>
 	);
