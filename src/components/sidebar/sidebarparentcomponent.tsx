b98786e20b62bada87f4f36605461cdb25f01026
James <james.lyons@dematic.com>
2024-08-13 04:11:57 +0100

    added highlighting in sidebar based on path

--- src/components/sidebar/sidebarparentcomponent.tsx
+++ src/components/sidebar/sidebarparentcomponent.tsx
@@ -1,7 +1,7 @@
 "use client";
 import Link from "next/link";
-import React from "react";
-
+import React, { useEffect, useState } from "react";
+import { usePathname, useSearchParams } from "next/navigation";
 interface SidebarparentcomponentProps {
 	text: string;
 	icon?: JSX.Element;
@@ -10,8 +10,29 @@ interface SidebarparentcomponentProps {
 }
 
 const Sidebarparentcomponent = (props: SidebarparentcomponentProps) => {
+	const pathname = usePathname();
+	const searchParams = useSearchParams();
+
+	const [highlighted, setHighlighted] = useState(false);
+
+	useEffect(() => {
+		const url = window.location.pathname;
+		const link = props.link;
+
+		if (link && url.startsWith(link) && link !== "/") {
+			setHighlighted(true);
+		} else if (url === link) {
+			setHighlighted(true);
+		} else {
+			setHighlighted(false);
+		}
+	}, [pathname, searchParams]);
+
 	const content = (
-		<div className="flex h-10 items-center px-0 py-0 text-gray-100 hover:bg-gray-700">
+		<div
+			className={`flex h-10 items-center px-0 py-0 text-gray-100 hover:bg-gray-600 ${highlighted ? "bg-gray-500" : ""}`}
+			title={props.text}
+		>
 			<button className="w-10 justify-start px-3 py-0">{props.icon}</button>
 
 			<div
