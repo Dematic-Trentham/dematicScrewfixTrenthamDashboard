d487ea96646824a84de0b434cb24068750b981f0
James <james.lyons@dematic.com>
2024-08-29 23:35:05 +0100

    main

---
+++ src/app/dashboard/shuttles/locations/page.tsx
@@ -0,0 +1,9 @@
+import PanelTop from "@/components/panels/panelTop";
+
+export default function Home() {
+	return (
+		<PanelTop className="w-full" title="Home">
+			<p>Home page content</p>
+		</PanelTop>
+	);
+}
