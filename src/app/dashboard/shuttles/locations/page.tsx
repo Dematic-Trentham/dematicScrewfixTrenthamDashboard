2db0642430510afc374a8698fb30720b8ac6581b
James <james.lyons@dematic.com>
2024-08-30 03:03:34 +0100

    123

--- src/app/dashboard/shuttles/locations/page.tsx
+++ src/app/dashboard/shuttles/locations/page.tsx
@@ -2,8 +2,14 @@ import PanelTop from "@/components/panels/panelTop";
 
 export default function Home() {
 	return (
-		<PanelTop className="w-full" title="Home">
-			<p>Home page content</p>
+		<PanelTop className="h-fit w-full" title="Shuttle Locations">
+			<div className="flex w-full flex-col">
+				<div>1</div>
+				<div>2</div>
+				<div>3</div>
+				<div>4</div>
+				<div>5</div>
+			</div>
 		</PanelTop>
 	);
 }
