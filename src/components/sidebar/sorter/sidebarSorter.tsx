2e0704f7845dcf28fee7632cdff6216463addfed
James <james.lyons@dematic.com>
2024-09-07 17:02:23 +0100

    init sorter sidebar

---
+++ src/components/sidebar/sorter/sidebarSorter.tsx
@@ -0,0 +1,29 @@
+"use server";
+import React from "react";
+import { FaBarsProgress } from "react-icons/fa6";
+
+import SidebarParentComponent from "../sidebarParentComponent";
+import SidebarParentComponentGroup from "../sidebarParentComponentGroup";
+
+import HorizontalBar from "@/components/visual/horizontalBar";
+
+const SidebarAdmin = async () => {
+	return (
+		<>
+			<HorizontalBar />
+			<SidebarParentComponentGroup
+				icon={<FaBarsProgress />}
+				link="/dashboard/sorter"
+				text="Sorter"
+			>
+				<SidebarParentComponent
+					icon={<FaBarsProgress />}
+					link="/dashboard/sorter/journeys"
+					text="Journey's"
+				/>
+			</SidebarParentComponentGroup>
+		</>
+	);
+};
+
+export default SidebarAdmin;
