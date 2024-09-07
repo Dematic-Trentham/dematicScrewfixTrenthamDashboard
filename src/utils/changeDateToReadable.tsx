12e7184048b88451f7cb2094a6dc4213173c22a5
James <james.lyons@dematic.com>
2024-09-07 17:04:20 +0100

    date utils

---
+++ src/utils/changeDateToReadable.tsx
@@ -0,0 +1,13 @@
+"use client";
+export const changeDateToReadable = (date: Date) => {
+	const newDate = new Date(date);
+
+	//HH:MM DD/MM/YYYY
+	const hours = newDate.getHours();
+	const minutes = newDate.getMinutes();
+	const day = newDate.getDate();
+	const month = newDate.getMonth() + 1;
+	const year = newDate.getFullYear();
+
+	return `${hours}:${minutes} ${day}/${month}/${year}`;
+};
