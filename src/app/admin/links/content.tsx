54ceda901355e67b233825d900d74d1c625f664b
James <james.lyons@dematic.com>
2024-09-21 16:23:33 +0100

    changed to open in nw tab

--- src/app/admin/links/content.tsx
+++ src/app/admin/links/content.tsx
@@ -7,15 +7,27 @@ const AdminLinksContent = () => {
 		<>
 			<PanelTop className="h-auto w-11/12" title="Admin Links">
 				<div className="flex flex-col space-y-2">
-					<Link className="text-l text-blue-400" href="http://10.4.5.227:9000/">
+					<Link
+						className="text-l text-blue-400"
+						href="http://10.4.5.227:9000/"
+						rel="noopener noreferrer"
+						target="_blank"
+					>
 						Portainer - Docker Containers
 					</Link>
-					<Link className="text-l text-blue-400" href="http://10.4.5.227:3080/">
+					<Link
+						className="text-l text-blue-400"
+						href="http://10.4.5.227:3080/"
+						rel="noopener noreferrer"
+						target="_blank"
+					>
 						Gitea - Local Git Server / Testing / CI / CD
 					</Link>
 					<Link
 						className="text-l text-blue-400"
-						href="http://http://10.4.5.227:8081/"
+						href="http://10.4.5.227:8081/"
+						rel="noopener noreferrer"
+						target="_blank"
 					>
 						Testing Dashboard - &apos;Testing&apos; Branch on Gitea
 					</Link>
