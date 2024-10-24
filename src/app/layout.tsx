876deb8483b069d34d9c1ec2940eed0b7df640de
James <james.lyons@dematic.com>
2024-10-24 09:44:30 +0100

    error page mods

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -41,7 +41,7 @@ export default function RootLayout({
 			<head />
 
 			<body className="antialiased">
-			<ErrorBoundary>
+		
 			<Suspense>
 				<CookiesProvider>
 					<Providers
@@ -64,7 +64,7 @@ export default function RootLayout({
 					</Providers>
 				</CookiesProvider>
 				</Suspense>
-				</ErrorBoundary>
+				
 			</body>
 		</html>
 	);
