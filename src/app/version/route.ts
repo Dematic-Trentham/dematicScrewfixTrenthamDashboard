c9213f2fbd01476fb4093c80677cceb3374a91d4
James Lyons <james.lyons@dematic.com>
2026-06-10 12:44:38 +0100

    feat: update version endpoint to return a unique app ID instead of OS version

--- src/app/version/route.ts
+++ src/app/version/route.ts
@@ -1,9 +1,14 @@
 import { NextResponse } from "next/server";
 
-import os from "os";
+import { v4 as uuidv4 } from "uuid";
+
+let appId: string;
 
 export async function GET() {
+	if (!appId) {
+		appId = uuidv4();
+	}
 	return NextResponse.json({
-		return: os.version(),
+		return: appId,
 	});
 }
